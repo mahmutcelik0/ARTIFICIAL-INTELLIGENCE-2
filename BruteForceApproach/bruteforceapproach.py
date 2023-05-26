e73defa68793eb8ee2e9ed196b0b01794dd66baf
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-05-26 13:44:25 +0300

    CELIK

--- BruteForceApproach/bruteforceapproach.py
+++ BruteForceApproach/bruteforceapproach.py
@@ -1,3 +1,38 @@
+import os.path
+import sys
+
+dictionary = {
+    "values": [],
+    "weights": [],
+    "knapsack_weight": 0,
+    "path": os.path.join("../DataSet", "1-Easy"),
+    "value_filename": "value_file.txt",
+    "weight_filename": "weight_file.txt",
+    "knapsack_filename": "knapsack_file.txt"
+}
+
+
+def fill_the_dictionary(hardness_level):
+    match hardness_level:
+        case 1:
+            dictionary["path"] = os.path.join("../DataSet", "1-Easy")
+        case 2:
+            dictionary["path"] = os.path.join("../DataSet", "2-Medium")
+        case 3:
+            dictionary["path"] = os.path.join("../DataSet", "3-Hard")
+
+    with open(os.path.join(dictionary["path"], dictionary["value_filename"]), "r") as file:
+        for line in file:
+            dictionary["values"].append(int(line.strip().split("\n")[0]))
+
+    with open(os.path.join(dictionary["path"], dictionary["weight_filename"]), "r") as file:
+        for line in file:
+            dictionary["weights"].append(int(line.strip().split("\n")[0]))
+
+    with open(os.path.join(dictionary["path"], dictionary["value_filename"]), "r") as file:
+        dictionary["knapsack_weight"] = int(file.read().strip().split("\n")[0])
+
+
 def solve_knapsack(profits, weights, capacity):
     return knapsack_recursive(profits, weights, capacity, 0)
 
@@ -21,8 +56,13 @@ def knapsack_recursive(profits, weights, capacity, currentIndex):
 
 
 def main():
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7)) # First part is value and the second part is weight array
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
+    sys.setrecursionlimit(10000)
+    fill_the_dictionary(3)
+    print(solve_knapsack(dictionary["values"], dictionary["weights"],
+                         dictionary["knapsack_weight"]))
+
+    # In python the max recursion limit is 1000 and the hardness level of 3 exceeds it.
+    # sys.setrecursionlimit(10000)
 
 
 main()
